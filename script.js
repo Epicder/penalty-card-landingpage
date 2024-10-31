1b7f940d14b7c62502b06d8876b9baba8d8c96fc
Epicder <epicder11@gmail.com>
2024-10-31 01:59:30 -0300

    Landing page v0.0.2 done ✔

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 function flipCard(element) {
   element.classList.toggle('flipped');
 }
+
+let slideIndex = 0;
+
+function showSlides() {
+    const slides = document.querySelectorAll('.carousel-slide img');
+    slides.forEach((slide, _) => {
+        slide.style.display = 'none';
+    });
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+function moveSlide(n) {
+    const slides = document.querySelectorAll('.carousel-slide img');
+    slideIndex += n;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+    slides.forEach((slide, _) => {
+        slide.style.display = 'none';
+    });
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlides();
+});
